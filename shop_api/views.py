ea936643a8d6d2a4b3c4b0ada411065e659c7caa
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-06-04 13:16:17 +0530

    Setup Viewset

--- shop_api/views.py
+++ shop_api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.response import Response 
 # Create your views here.
+ class ListViewSet(viewsets.ViewSet):
+     def list(self,request):
+         return Response("Success!")
