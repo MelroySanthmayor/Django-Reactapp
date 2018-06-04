bc91291d3c2ec723752a4a8e4d30ca115c3fb908
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-06-04 13:26:51 +0530

    Indent Correction

--- shop_api/views.py
+++ shop_api/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response 
 # Create your views here.
- class ListViewSet(viewsets.ViewSet):
-     def list(self,request):
-         return Response("Success!")
+class ListViewSet(viewsets.ViewSet):
+    def list(self,request):
+        return Response("Success!")
