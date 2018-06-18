905b00eb103c44a3918af83df4bffbd650602b37
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-06-18 15:15:50 +0530

    added suer and rating views and added a method in the rating view

--- shop_api/views.py
+++ shop_api/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response 
-from .models import Item
-from .serializers import ItemSerializer
+from .models import Item,User,Rating
+from .serializers import ItemSerializer,UserSerializer,RatingSerializer
+from rest_framework.permissions import IsAdminUser,IsAuthenticated,IsAuthenticatedOrReadOnly
+
+
+
 # Create your views here.
 class ListViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = (IsAdminUser,)
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+
+    
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+    def avg_rating(self):
+            #rates = Rating.objects.filter(item__)
+            rate = Item.objects.annotate(average_rating = Avg('Rating__rating'))
+            rate.save()
+            return rate
